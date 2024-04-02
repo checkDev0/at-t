ab07f2c19a53079c901b70b5c92039bd585ea77b
Azeem-py <74514285+Azeem-py@users.noreply.github.com>
2024-04-02 14:02:17 +0100

    chnge

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -1,2 +1,2 @@
-// export const baseURL = 'http://localhost:3000/'
-export const baseURL = 'https://fr-1290.vercel.app/'
+export const baseURL = 'http://localhost:3000/'
+// export const baseURL = 'https://fr-1290.vercel.app/'
